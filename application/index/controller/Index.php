bcd412241650f11c6a96b098b761e14b139fb0eb
cbwfree <cbwfree@163.com>
2016-07-07 16:00:47 +0800

    init commit

---
+++ application/index/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 文件描述
+ * Created by PhpStorm.
+ * @package app\home\controller
+ * @version 16/7/7 下午2:54
+ * @author  cbwfree
+ */
+namespace app\home\controller;
+
+use think\Controller;
+
+/**
+ * 文件描述
+ * Class Index
+ * @package app\home\controller
+ */
+class Index extends Controller
+{
+
+}
