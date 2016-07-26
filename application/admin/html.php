db9fedd0c7b2388b7f60d3ac2b01779d84c3d620
cbwfree <cbwfree@163.com>
2016-07-26 17:46:02 +0800

    调整后台UI

---
+++ application/admin/html.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin模块HTML函数
+ * Created by PhpStorm.
+ * @author  cbwfree
+ */
+
+/**
+ * 创建导航条
+ * @param array $path
+ * @return string
+ */
+function location(array $path = []): string
+{
+    $location = '';
+
+
+    return $location;
+}
