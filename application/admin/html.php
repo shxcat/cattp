b4d702d6ef10fbc7d99e88d4d2bea7ae6aa5b9f5
cbwfree <cbwfree@163.com>
2016-08-11 01:22:49 +0800

    模版库调整

--- application/admin/html.php
+++ application/admin/html.php
@@ -6,35 +6,6 @@
  */
 
 /**
- * 创建导航条
- * @param array $path       导航条路径
- * @param bool  $tools      导航条工具按钮
- * @return string
- */
-function location(array $path = [], $tools = true)
-{
-    $location = [
-        '<ol id="location" class="am-breadcrumb am-breadcrumb-slash">',
-        '<li><a href="'.url('admin/index/index').'" class="am-icon-home" data-pjax>首页</a></li>'
-    ];
-    foreach($path as $key => $val){
-        if( is_string($key) ){
-            $location[] = '<li><a href="'.url($key).'" data-pjax>'.$val.'</a></li>';
-        }else{
-            $location[] = '<li>'.$val.'</li>';
-        }
-    }
-
-    if ($tools) {
-        $location[] = '<li class="am-fr"><a href="javascript:;" onclick="$.AMUI.pjax.reload();" title="刷新"><i class="am-icon-refresh"></i>刷新(F5)</a></li>';
-        $location[] = '<li class="am-fr"><a href="javascript:;" onclick="window.open($.AMUI.pjax.location());" title="新窗口打开"><i class="am-icon-external-link"></i>新窗口打开</a></li>';
-    }
-
-    $location[] = '</ol>';
-    return implode("", $location);
-}
-
-/**
  * 返回 Tree 缩进信息
  * @param int    $level     缩进等级
  * @param string $space     缩进空白
