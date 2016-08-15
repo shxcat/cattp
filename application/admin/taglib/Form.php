a4e12355349d71679d74d947540b04f0ef526bbe
cbwfree <cbwfree@163.com>
2016-08-15 15:56:21 +0800

    表单模板优化

--- application/admin/taglib/Form.php
+++ application/admin/taglib/Form.php
@@ -26,16 +26,22 @@ class Form extends TagLib
         'form'      => ['attr' => 'submit,class,valid', 'expression' => true, 'level' => 0],
         'fieldset'  => ['attr' => 'class,title', 'expression' => true, 'level' => 0],
         'label'     => ['attr' => 'label,width,help'],
-        'input'     => ['attr' => 'label,width,help,valid,default,name,type,value,class,tips,help,attr', 'close' => 0],
-        'select'    => ['attr' => 'label,width,help,valid,default,name,options,fields,value,class,first,remote,multi,change,attr', 'close' => 0],
-        'text'      => ['attr' => 'label,width,help,valid,default,name,value,class,height,tips,attr', 'close' => 0],
+        'input'     => ['attr' => 'label,width,help,valid,default,id,name,type,value,class,tips,help,attr', 'close' => 0],
+        'select'    => ['attr' => 'label,width,help,valid,default,id,name,options,fields,value,class,first,remote,multi,attr', 'close' => 0],
+        'text'      => ['attr' => 'label,width,help,valid,default,id,name,value,class,height,tips,attr', 'close' => 0],
         'checkbox'  => ['attr' => 'label,width,help,valid,default,name,options,value,class,attr', 'close' => 0],
         'radio'     => ['attr' => 'label,width,help,valid,default,name,options,value,class,attr', 'close' => 0],
+        'hidden'    => ['attr' => 'label,width,help,valid,default,id,name,value', 'close' => 0],
     ];
 
     /**
      * 创建表单
      * @param $tag
+     *      submit      表单提交URL, 支持变量和函数
+     *      class       Class属性
+     *      method      表单提交方式, 可选 post 或 get
+     *      ajax        是否Ajax方式提交表单
+     *      valid       是否开启表单验证
      * @param $content
      * @return string
      */
@@ -72,6 +78,8 @@ class Form extends TagLib
     /**
      * 表单 Fieldset
      * @param $tag
+     *      class       Fieldset Class 样式
+     *      title       Fieldset 标题内容
      * @param $content
      * @return string
      */
@@ -94,6 +102,8 @@ class Form extends TagLib
     /**
      * 创建Label, 自定义输入框内容
      * @param $tag
+     *      width       输入框容器宽度, 范围 1 ~ 10
+     *      help        帮助信息
      * @param $content
      * @return string
      */
@@ -135,19 +145,31 @@ class Form extends TagLib
     /**
      * 创建输入框
      * @param $tag
+     *      name        Input name属性
+     *      id          Input id属性
+     *      type        Input type属性
+     *      value       Input value属性
+     *      class       Input class属性
+     *      tips        Input 占位内容
+     *      attr        Input 额外属性或自定义属性
+     *      default     Input 默认value属性
+     *      valid       Input 表单验证规则
      * @return string
      */
     public function tagInput($tag)
     {
         // name,type,value,class,tips,help,attr
         $name   = $tag['name'];
+        $id     = isset($tag['id']) ? ' id="'.$tag['id'].'"' : '';
         $type   = isset($tag['type']) ? $tag['type'] : 'text';
         $value  = isset($tag['value']) ? $tag['value'] : '';
         $class  = isset($tag['class']) ? $tag['class'] : '';
         $tips   = isset($tag['tips']) ? $tag['tips'] : '';
         $attr   = isset($tag['attr']) ? ' '.$tag['attr'] : '';
         $default= isset($tag['default']) ? $tag['default'] : '';
-        $valid  = isset($tag['valid']) ? $this->formValid($tag['valid']) : '';
+
+        // 表单验证
+        $valid  = $this->formValid(isset($tag['valid']) ? $tag['valid'] : '');
 
         // 支持用函数传数组
         $flag  = substr($value, 0, 1);
@@ -158,7 +180,33 @@ class Form extends TagLib
             $value = '<?php echo isset('.$value.') ? '.$value.': "'.$default.'";?>';
         }
 
-        $content = '<input type="'.$type.'" name="'.$name.'" value="'.$value.'" class="am-form-field '.$class.'" placeholder="'.$tips.'"'.$attr.$valid.'>';
+        $content = '<input type="'.$type.'"'.$id.' name="'.$name.'" value="'.$value.'" class="am-form-field '.$class.$valid['class'].'" placeholder="'.$tips.'"'.$attr.$valid['rule'].' />';
+
+        return $this->tagLabel($tag, $content);
+    }
+
+    /**
+     * 创建隐藏域
+     * @param $tag
+     * @return string
+     */
+    public function tagHidden($tag)
+    {
+        $name   = $tag['name'];
+        $id     = isset($tag['id']) ? ' id="'.$tag['id'].'"' : '';
+        $value  = isset($tag['value']) ? $tag['value'] : '';
+        $default= isset($tag['default']) ? $tag['default'] : '';
+
+        // 支持用函数传数组
+        $flag  = substr($value, 0, 1);
+        if (":" == $flag) {
+            $value = '<?php echo '.$this->autoBuildVar($value).'; ?>';
+        } elseif ("$" == $flag) {
+            $value = $this->autoBuildVar($value);
+            $value = '<?php echo isset('.$value.') ? '.$value.': "'.$default.'";?>';
+        }
+
+        $content = '<input type="hidden"'.$id.' name="'.$name.'" value="'.$value.'" />';
 
         return $this->tagLabel($tag, $content);
     }
@@ -166,19 +214,31 @@ class Form extends TagLib
     /**
      * 创建文本输入框
      * @param $tag
+     *      name        Textarea name属性
+     *      id          Textarea id属性
+     *      value       Textarea 内容
+     *      class       Textarea class属性
+     *      tips        Textarea 占位内容
+     *      attr        Textarea 额外属性或自定义属性
+     *      height      Textarea 高度
+     *      default     Textarea 默认内容
+     *      valid       Textarea 表单验证规则
      * @return string
      */
     public function tagText($tag)
     {
         // name,type,value,class,tips,help,attr
         $name   = $tag['name'];
+        $id     = isset($tag['id']) ? ' id="'.$tag['id'].'"' : '';
         $value  = isset($tag['value']) ? $tag['value'] : '';
         $class  = isset($tag['class']) ? $tag['class'] : '';
         $tips   = isset($tag['tips']) ? $tag['tips'] : '';
         $attr   = isset($tag['attr']) ? ' '.$tag['attr'] : '';
         $height = isset($tag['height']) ? 'height:'.$tag['height'].'px;' : '';
         $default= isset($tag['default']) ? $tag['default'] : '';
-        $valid  = isset($tag['valid']) ? $this->formValid($tag['valid']) : '';
+
+        // 表单验证
+        $valid  = $this->formValid(isset($tag['valid']) ? $tag['valid'] : '');
 
         // 支持用函数传数组
         $flag  = substr($value, 0, 1);
@@ -189,7 +249,7 @@ class Form extends TagLib
             $value = '<?php echo isset('.$value.') ? '.$value.': "'.$default.'";?>';
         }
 
-        $content = '<textarea name="'.$name.'" class="am-form-field'.$class.'" style="'.$height.'" placeholder="'.$tips.'"'.$attr.$valid.'>'.$value.'</textarea>';
+        $content = '<textarea name="'.$name.'"'.$id.' class="am-form-field'.$class.$valid['class'].'" style="'.$height.'" placeholder="'.$tips.'"'.$attr.$valid['rule'].'>'.$value.'</textarea>';
 
         return $this->tagLabel($tag, $content);
     }
@@ -197,12 +257,24 @@ class Form extends TagLib
     /**
      * 创建选择列表
      * @param $tag
+     *      name        Select name属性
+     *      id          Select id属性
+     *      value       Select 当前选中值, 多选时
+     *      class       Select class属性
+     *      first       Select 占位内容(空选项)
+     *      options     Select 选项列表
+     *      remote      Select2 Ajax请求URL
+     *      attr        Select 额外属性或自定义属性
+     *      multi       Select 是否多选列表
+     *      default     Select 默认选择
+     *      valid       Select 表单验证规则
      * @return string
      */
     public function tagSelect($tag)
     {
         // name,options,value,class,attr,first
         $name   = $tag['name'];
+        $id     = isset($tag['id']) ? ' id="'.$tag['id'].'"' : '';
         $value  = isset($tag['value']) ? $tag['value'] : '';
         $class  = isset($tag['class']) ? ' '.$tag['class'] : '';
         $first  = isset($tag['first']) ? $tag['first'] : '';
@@ -210,43 +282,52 @@ class Form extends TagLib
         $remote = isset($tag['remote']) ? $tag['remote'] : '';
         $attr   = isset($tag['attr']) ? ' '.$tag['attr'] : '';
         $multi  = isset($tag['multi']) ? ' multiple' : '';
-        $change = isset($tag['change']) ? ' onchange="'.$tag['change'].'"' : '';
         $fields = isset($tag['fields']) ? explode(',', $tag['fields']) : '';
         $default= isset($tag['default']) ? $tag['default'] : '';
-        $valid  = isset($tag['valid']) ? $this->formValid($tag['valid']) : '';
 
+        // 表单验证
+        $valid  = $this->formValid(isset($tag['valid']) ? $tag['valid'] : '');
 
-        if (strpos($attr, "data-select")) {
+        // 处理 Select2 扩展
+        if (strpos($attr, "data-select") !== false) {
             if ($first) {
                 $attr.= ' data-placeholder="'.$first.'" data-allow-clear="true"';
                 $first = '<option></option>';
             }
 
             if (isset($remote)) {
+                // AjaxURL参数解析
+                $flag  = substr($remote, 0, 1);
+                if (":" == $flag || "$" == $flag) {
+                    $remote = '<?php echo '.$this->autoBuildVar($remote).'; ?>';
+                }
                 $attr.= ' data-remote="'.$remote.'"';
             }
         } elseif ($first) {
             $first = '<option>'.$first.'</option>';
         }
 
-        $content = '<select name="'.$name.'" class="am-form-field'.$class.'"'.$change.$multi.$attr.$valid.'>';
+        $content = '<select'.$id.' name="'.$name.'" class="am-form-field'.$class.$valid['class'].'"'.$multi.$attr.$valid['rule'].'>';
         $content.= $first;
         $content.= '<?php ';
 
-        // 支持用函数传数组
-        if (strpos($value, ":") === 0) {
+        // 选中项参数解析, 支持用函数传数组
+        $flag  = substr($value, 0, 1);
+        if (":" == $flag) {
             $var = '$_' . uniqid();
             $value = $this->autoBuildVar($value);
             $content.= $var . '=' . $value . '; ';
             $value = $var;
-        } elseif (strpos($value, "$") === 0) {
+        } elseif ("$" == $flag) {
             $value = $this->autoBuildVar($value);
-            $content.= $value . ' = isset('. $value .') ? '. $value. ' : "'.$default.'"; ';
+            $content .= $value . ' = isset(' . $value . ') ? ' . $value . ' : "' . $default . '"; ';
+        } elseif ("[" == $flag) {
+            $value = '';
         } else{
             $value = '\''.$value.'\'';
         }
 
-        // 支持用函数传数组
+        // 选项列表参数解析, 支持用函数传数组
         $flag  = substr($options, 0, 1);
         if (":" == $flag) {
             $var = '$_' . uniqid();
@@ -276,6 +357,12 @@ class Form extends TagLib
     /**
      * 创建多选框列表
      * @param $tag
+     *      name        Checkbox name属性
+     *      value       Checkbox 选中项值
+     *      class       Checkbox class属性
+     *      options     Checkbox 选项数组, 支持变量或函数
+     *      default     Checkbox 默认选中项
+     *      valid       Checkbox 表单验证规则
      * @return string
      */
     public function tagCheckbox($tag)
@@ -285,9 +372,10 @@ class Form extends TagLib
         $value  = isset($tag['value']) ? $tag['value'] : '';
         $class  = isset($tag['class']) ? $tag['class'] : '';
         $options= isset($tag['options']) ? $tag['options'] : [];
-        $change = isset($tag['change']) ? ' onchange="'.$tag['change'].'"' : '';
         $default= isset($tag['default']) ? $tag['default'] : '';
-        $valid  = isset($tag['valid']) ? $this->formValid($tag['valid']) : '';
+
+        // 表单验证
+        $valid  = $this->formValid(isset($tag['valid']) ? $tag['valid'] : '');
 
         $content = '<?php ';
 
@@ -324,10 +412,11 @@ class Form extends TagLib
         $content.= 'if(is_array(' . $options . ') || ' . $options . ' instanceof \think\Collection): ';
         $content.= '$_index = 0;';
         $content.= 'foreach ('. $options .' as $k => $v): ';
-        $content.= '$_valid = $_index == 0 ? "'.$valid.'" : ""; ';
+        $content.= 'if ($_index === 0): $_valid_rule = "'.$valid['rule'].'"; $_valid_class = "'.$valid['class'].'"; ';
+        $content.= 'else: $_valid_rule = ""; $_valid_class = ""; endif;';
         $content.= '$_checked = (($k == '.$value.' && '.$value.' != "") || (is_array('.$value.') && in_array($k, '.$value.'))) ? " checked" : ""; ?>';
         $content.= '<label class="am-checkbox-inline'.$class.'">';
-        $content.= '<input type="checkbox" name="'.$name.'" value="<?php echo $k; ?>"<?php echo $_valid; ?>'.$change.'<?php echo $_checked;?>> <?php echo $v; ?>';
+        $content.= '<input type="checkbox" name="'.$name.'" class="<?php echo $_valid_class; ?>" value="<?php echo $k; ?>"<?php echo $_valid_rule; ?><?php echo $_checked;?> /> <?php echo $v; ?>';
         $content.= '</label>';
         $content.= '<?php $_index++; endforeach; endif; ?>';
 
@@ -337,6 +426,12 @@ class Form extends TagLib
     /**
      * 创建单选框列表
      * @param $tag
+     *      name        Radio name属性
+     *      value       Radio 选中项值
+     *      class       Radio class属性
+     *      options     Radio 选项数组, 支持变量或函数
+     *      default     Radio 默认选中项
+     *      valid       Radio 表单验证规则
      * @return string
      */
     public function tagRadio($tag)
@@ -348,7 +443,9 @@ class Form extends TagLib
         $options= isset($tag['options']) ? $tag['options'] : [];
         $change = isset($tag['change']) ? ' onchange="'.$tag['change'].'"' : '';
         $default= isset($tag['default']) ? $tag['default'] : '';
-        $valid  = isset($tag['valid']) ? $this->formValid($tag['valid']) : '';
+
+        // 表单验证
+        $valid  = $this->formValid(isset($tag['valid']) ? $tag['valid'] : '');
 
         $content = '<?php ';
 
@@ -382,10 +479,11 @@ class Form extends TagLib
         $content.= 'if(is_array(' . $options . ') || ' . $options . ' instanceof \think\Collection): ';
         $content.= '$_index = 0;';
         $content.= 'foreach ('. $options .' as $k => $v): ';
-        $content.= '$_valid = $_index == 0 ? "'.$valid.'" : ""; ';
+        $content.= 'if ($_index === 0): $_valid_rule = "'.$valid['rule'].'"; $_valid_class = "'.$valid['class'].'"; ';
+        $content.= 'else: $_valid_rule = ""; $_valid_class = ""; endif;';
         $content.= '$_checked = ($k == '.$value.' && '.$value.' != "") ? " checked" : ""; ?>';
         $content.= '<label class="am-radio-inline'.$class.'">';
-        $content.= '<input type="radio" name="'.$name.'" value="<?php echo $k; ?>"<?php echo $_valid; ?>'.$change.'<?php echo $_checked;?>> <?php echo $v; ?>';
+        $content.= '<input type="radio" name="'.$name.'" class="<?php echo $_valid_class; ?>"  value="<?php echo $k; ?>"<?php echo $_valid_rule; ?><?php echo $_checked;?> /> <?php echo $v; ?>';
         $content.= '</label>';
         $content.= '<?php $_index++; endforeach; endif; ?>';
 
@@ -393,69 +491,86 @@ class Form extends TagLib
     }
 
     /**
-     * 生成表单验证
-     * @param $rules
+     * 表单验证规则解析
+     * @param string $rules 规则字符串, 格式: 规则名称:规则属性|规则名称:规则属性, 例如: required|length:3,4
+     *      required               必填
+     *      equal:选择器            匹配指定字段的值
+     *      regex:正则              使用正则表达式
+     *      min:数值               最小值
+     *      max:数值               最大值
+     *      length:数值,数值        长度范围
+     *      checked:数值,数值       选择项范围
      * @return string
      */
     protected function formValid($rules)
     {
-        $valid = [];
+        $valid = [
+            'rule'      => [],
+            'class'     => [],
+        ];
 
         if (! empty($rules)) {
             $rules = explode("|", $rules);
 
             foreach ($rules as $rule) {
                 if ($rule == "required") {
-                    $valid[] = "required";
+                    $valid['rule'][] = "required";
                 } elseif (strpos($rule, ":")) {
-                    $rule = explode(":", $rule);
+                    $rule = explode(":", $rule, 2);
 
                     switch($rule[0]) {
                         // 匹配其它字段
                         case 'equal':
-                            $valid[] = 'data-equal-to="'.$rule[1].'"';
+                            $valid['rule'][] = 'data-equal-to="'.$rule[1].'"';
                             break;
                         // 正则
                         case 'regex':
-                            $valid[] = 'pattern="'.$rule[1].'"';
+                            $valid['rule'][] = 'pattern="'.$rule[1].'"';
                             break;
                         // 最小值
                         case 'min':
                             if (is_numeric($rule[1])) {
-                                $valid[] = 'min="'.$rule[1].'"';
+                                $valid['rule'][] = 'min="'.$rule[1].'"';
                             }
                             break;
                         // 最大值
                         case 'max':
                             if (is_numeric($rule[1])) {
-                                $valid[] = 'max="'.$rule[1].'"';
+                                $valid['rule'][] = 'max="'.$rule[1].'"';
                             }
                             break;
                         // 字符长度范围
                         case 'length':
                             list($min, $max) = explode(",", $rule[1]);
                             if (is_numeric($min)) {
-                                $valid[] = 'minlength="'.$min.'"';
+                                $valid['rule'][] = 'minlength="'.$min.'"';
                             }
                             if (is_numeric($max)) {
-                                $valid[] = 'maxlength="'.$max.'"';
+                                $valid['rule'][] = 'maxlength="'.$max.'"';
                             }
                             break;
                         // 选择项数量范围
                         case 'checked':
                             list($min, $max) = explode(",", $rule[1]);
                             if (is_numeric($min)) {
-                                $valid[] = 'minchecked="'.$min.'"';
+                                $valid['rule'][] = 'minchecked="'.$min.'"';
                             }
                             if (is_numeric($max)) {
-                                $valid[] = 'maxchecked="'.$max.'"';
+                                $valid['rule'][] = 'maxchecked="'.$max.'"';
                             }
                             break;
                     }
+                } else {
+                    // 使用 AMUI 定义的正则库验证
+                    $valid['class'][] = "js-pattern-" . $rule;
                 }
             }
         }
 
-        return $valid ? " " . implode(" ", $valid) : "";
+
+        $valid['rule'] = $valid['rule'] ? " " . implode(" ", $valid['rule']) : "";
+        $valid['class'] = $valid['class'] ? " " . implode(" ", $valid['class']) : "";
+
+        return $valid;
     }
 }
